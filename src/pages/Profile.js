0bff8d98a95ce48bab16c8a296743ae02fde68eb
susankhatri00 <susankhatri00@gmail.com>
2024-01-25 13:04:59 +0545

    adding all last functionality like authorization

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,13 +1,51 @@
-import { useHistory } from "react-router-dom";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Link, useHistory } from "react-router-dom";
 
 const Profile = () => {
+  const [userData, setUserData] = useState({});
   const history = useHistory();
-  const onLogout = () => {
+  useEffect(() => {
+    getProfile();
+  }, []);
+
+  const getProfile = async () => {
+    const getAccessToken = localStorage.getItem("accessToken");
+    try {
+      const response = await axios.get(
+        "https://api.dynoacademy.com/test-api/v1/me",
+
+        {
+          timeout: 10000,
+          headers: {
+            Authorization: `Bearer ${getAccessToken}`,
+          },
+        }
+      );
+      console.log(response);
+      setUserData(response.data.data);
+    } catch (error) {
+      if (error.response) {
+        alert(error.response.data.errors[0].message);
+      } else {
+        alert("unknown error try again boy");
+      }
+    }
+  };
+  const onLogout = async () => {
     localStorage.removeItem("accessToken");
     history.replace("/");
   };
   return (
     <>
+      <Link to="/">Home</Link>
+      <br />
+      Name:{userData.name}
+      <br />
+      Email:{userData.email}
+      <br />
+      Country:{userData.country}
+      <br />
       <button onClick={onLogout}>Logout</button>
     </>
   );
